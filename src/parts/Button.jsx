02527adaf5df66651fcddbed0600d02514a410a1
Naomi Earle <naomi.earle@highspeedtraining.co.uk>
2023-06-05 08:23:05 +0100

    button props set up correctly

--- src/parts/Button.jsx
+++ src/parts/Button.jsx
@@ -1,16 +1,21 @@
+import PropTypes from 'prop-types'
+import emailIcon from '../assets/email.png'
+import linkedinIcon from '../assets/linkedin.png'
+
 export default function Button({ linkSrc, color, text, icon }) {
   const className = `button button-${color}`
-  let iconSrc;
-  if (icon === 'email') {
-    iconSrc = 'email-icon';
-  } else if (icon === 'linkedin') {
-    iconSrc = 'linkedin-icon';
-  }
   
   return (
     <a className={className} href={linkSrc}>
-      <p>{iconSrc}</p>
+      <img src={icon === 'email' ? emailIcon : linkedinIcon} />
       <p>{text}</p>
     </a>
   )
+}
+
+Button.propTypes = {
+  linkSrc: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired
 }
