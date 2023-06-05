411feace5576b44c7e976762f88076c031662573
Naomi Earle <naomi.earle@highspeedtraining.co.uk>
2023-06-05 15:26:53 +0100

    show/hide content

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ Project for Scrimba's "Learn React" course: https://scrimba.com/learn/learnreact
 
 Notes:
 1. Figma design file available here: https://www.figma.com/file/oJGgx3gfO2SMIMDiAhbIP3/Digital-Business-Card?type=design&node-id=0%3A129&t=ehwNtYW7OYv5ua4A-1
+
+
+Things to do next:
+1. Light/dark mode
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,11 +1,27 @@
+import { useState } from "react"
+
 import ContentSection from "../parts/ContentSection"
 import "../assets/css/Content.css"
 
 export default function Content() {
+  const [showContent, setShowContent] = useState(false)
+
+  const toggleShowContent = () => {
+    setShowContent(!showContent)
+  }
+
+  const iconClassName = showContent === true ? "fa-solid fa-chevron-up" : "fa-solid fa-chevron-down"
+  const iconText = showContent === true ? "Hide info" : "Show info"
+
   return (
     <div className="content">
-      <ContentSection type="about" />
-      <ContentSection type="interests" />
+      <button className="content_toggle-button" onClick={toggleShowContent}>{iconText} <i className={iconClassName}></i></button>
+      {showContent === true &&
+        <>
+          <ContentSection type="about" />
+          <ContentSection type="interests" />
+        </>
+      }
     </div>
   )
 }
